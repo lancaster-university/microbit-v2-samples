2ffb0c1619c04d313fb82cba8944a9ce1c3f75dd
James Devine <j.devine@lancaster.ac.uk>
2017-06-08 10:09:49 +0100

    Fixed windows builds after toolchain removed from main CMake
    I previously moved all toolchain related cmake out into separate files
    for inclusion at particular points. Compiler flags are required to be
    included after project(), this has now been enforced.

--- utils/cmake/toolchains/ARM_GCC/toolchain.cmake
+++ utils/cmake/toolchains/ARM_GCC/toolchain.cmake
@@ -23,39 +23,3 @@ endif()
 SET(CMAKE_AR "${ARM_NONE_EABI_AR}" CACHE FILEPATH "Archiver")
 SET(CMAKE_RANLIB "${ARM_NONE_EABI_RANLIB}" CACHE FILEPATH "rlib")
 set(CMAKE_CXX_OUTPUT_EXTENSION ".o")
-
-# Override the link rules:
-set(CMAKE_C_CREATE_SHARED_LIBRARY "echo 'shared libraries not supported' && 1")
-set(CMAKE_C_CREATE_SHARED_MODULE  "echo 'shared modules not supported' && 1")
-set(CMAKE_C_CREATE_STATIC_LIBRARY "<CMAKE_AR> -cr <LINK_FLAGS> <TARGET> <OBJECTS>")
-set(CMAKE_C_COMPILE_OBJECT        "<CMAKE_C_COMPILER> <DEFINES> ${EXPLICIT_INCLUDES}<FLAGS> -o <OBJECT> -c <SOURCE>")
-
-set(CMAKE_C_LINK_EXECUTABLE       "<CMAKE_C_COMPILER> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> -Wl,-Map,<TARGET>.map -Wl,--start-group <OBJECTS> <LINK_LIBRARIES> -lm -lc -lgcc -lm -lc -lgcc -Wl,--end-group --specs=nano.specs -o <TARGET>")
-
-set(CMAKE_CXX_OUTPUT_EXTENSION ".o")
-set(CMAKE_DEPFILE_FLAGS_CXX "-MMD -MT <OBJECT> -MF <DEPFILE>")
-set(CMAKE_C_OUTPUT_EXTENSION ".o")
-set(CMAKE_DEPFILE_FLAGS_C "-MMD -MT <OBJECT> -MF <DEPFILE>")
-
-set(CMAKE_C_FLAGS_DEBUG_INIT          "-g -gdwarf-3")
-set(CMAKE_C_FLAGS_MINSIZEREL_INIT     "-Os -DNDEBUG")
-set(CMAKE_C_FLAGS_RELEASE_INIT        "-Os -DNDEBUG")
-set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "-Os -g -gdwarf-3 -DNDEBUG")
-set(CMAKE_INCLUDE_SYSTEM_FLAG_C "-isystem ")
-
-
-set(CMAKE_ASM_FLAGS_DEBUG_INIT          "-g -gdwarf-3")
-set(CMAKE_ASM_FLAGS_MINSIZEREL_INIT     "-Os -DNDEBUG")
-set(CMAKE_ASM_FLAGS_RELEASE_INIT        "-Os -DNDEBUG")
-set(CMAKE_ASM_FLAGS_RELWITHDEBINFO_INIT "-Os -g -gdwarf-3 -DNDEBUG")
-set(CMAKE_INCLUDE_SYSTEM_FLAG_ASM  "-isystem ")
-
-set(CMAKE_CXX_CREATE_STATIC_LIBRARY "<CMAKE_AR> -cr <LINK_FLAGS> <TARGET> <OBJECTS>")
-
-set(CMAKE_CXX_LINK_EXECUTABLE       "<CMAKE_CXX_COMPILER> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> -Wl,-Map,<TARGET>.map -Wl,--start-group <OBJECTS> <LINK_LIBRARIES> -lnosys -lstdc++ -lsupc++ -lm -lc -lgcc -lstdc++ -lsupc++ -lm -lc -lgcc -Wl,--end-group  --specs=nano.specs -o <TARGET>")
-
-set(CMAKE_CXX_FLAGS_DEBUG_INIT          "-g -gdwarf-3")
-set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "-Os -DNDEBUG")
-set(CMAKE_CXX_FLAGS_RELEASE_INIT        "-Os -DNDEBUG")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-Os -g -gdwarf-3 -DNDEBUG")
-set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "-isystem ")
