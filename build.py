d2b2b15c1b1a47c197043c4ba36d22a25c1c88de
James Devine <j.devine@lancaster.ac.uk>
2017-12-02 09:48:47 +0000

    added new test_ignore flag to skip platforms that do not have travis support.

--- build.py
+++ build.py
@@ -246,6 +246,10 @@ if not options.test_platform:
 
 for json_obj in test_json:
 
+    # some platforms aren't supported by travis, ignore them when testing.
+    if json_obj["test_ignore"] == True:
+        continue
+
     # ensure we have a clean build tree.
     delete_build_folder()
 
