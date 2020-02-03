2f639216dedcb1c20374b9e206e6c14d91fa2b23
Raphael Gault <raphael.gault@arm.com>
2020-02-03 14:12:40 +0000

    Fix include of MicroBit.h in SerialStreamer.h
    Signed-off-by: Raphael Gault <raphael.gault@arm.com>

--- source/SerialStreamer.h
+++ source/SerialStreamer.h
@@ -22,7 +22,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-#include "Microbit.h"
+#include "MicroBit.h"
 #include "CodalConfig.h"
 #include "DataStream.h"
 
