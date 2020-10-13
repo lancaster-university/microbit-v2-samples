98f6ccf6589b6946e5d436a9db54545faae9d3b1
Joe Finney <joe@comp.lancs.ac.uk>
2020-10-13 11:28:49 +0100

    Disable Github workflows whilst migrating

--- .github/workflows/scripts/install-toolchain-win.ps1
+++
@@ -1,7 +0,0 @@
-# Use chocolatey to install Arm GCC, CMake, and Ninja
-choco install gcc-arm-embedded
-choco install cmake
-choco install ninja
-# Install srec by downloading it and unzipping into the cwd
-C:\msys64\usr\bin\wget.exe -O srecord.zip https://sourceforge.net/projects/srecord/files/srecord-win32/1.64/srecord-1.64-win32.zip/download
-Expand-Archive -Force srecord.zip .
