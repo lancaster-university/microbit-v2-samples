20c8213318e510dfd64ace66a5d2d02a5d5d3d6a
Sam Kent <sam@microbit.org>
2021-01-08 22:36:02 +0000

    test neopixel sample for sparklet

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include "MicroBit.h"
+#include "neopixel.h"
 
 MicroBit uBit;
 
@@ -6,7 +7,23 @@ int
 main()
 {
     uBit.init();
+    uint8_t buffer[24] = {
+        0xFF, 0x00, 0x00,
+        0xFF, 0x00, 0x00,
+        0xFF, 0x00, 0x00,
+        0xFF, 0x00, 0x00,
+        0xFF, 0x00, 0x00,
+        0xFF, 0x00, 0x00,
+        0xFF, 0x00, 0x00,
+        0xFF, 0x00, 0x00,
+    };
 
-    while(1)
+    uBit.io.P1.setDigitalValue(1);
+
+    uBit.io.P0.setHighDrive(true);
+    neopixel_send_buffer(uBit.io.P0, buffer, 24);
+
+    while(1) {
         uBit.display.scroll("HELLO WORLD!");
+    }
 }
