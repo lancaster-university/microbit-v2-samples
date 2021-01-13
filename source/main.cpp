ecbcf41b67acf7a55080cf0ef444df51f35d209a
Sam Kent <sam@microbit.org>
2021-01-13 16:57:40 +0000

    define for n neopixels

--- source/main.cpp
+++ source/main.cpp
@@ -3,25 +3,24 @@
 
 MicroBit uBit;
 
+#define NUM_OF_NEOPIXELS 256
+
 int 
 main()
 {
     uBit.init();
-    uint8_t buffer[24] = {
-        0xFF, 0x00, 0x00,
-        0xFF, 0x00, 0x00,
-        0xFF, 0x00, 0x00,
-        0xFF, 0x00, 0x00,
-        0xFF, 0x00, 0x00,
-        0xFF, 0x00, 0x00,
-        0xFF, 0x00, 0x00,
-        0xFF, 0x00, 0x00,
-    };
+    uint8_t buffer[3 * NUM_OF_NEOPIXELS];
 
     uBit.io.P1.setDigitalValue(1);
-
     uBit.io.P0.setHighDrive(true);
-    neopixel_send_buffer(uBit.io.P0, buffer, 24);
+
+    for(int i = 0; i < NUM_OF_NEOPIXELS; i++) {
+        buffer[3*i    ] = 0xff;
+        buffer[3*i + 1] = 0x00;
+        buffer[3*i + 2] = 0x00;
+    }
+
+    neopixel_send_buffer(uBit.io.P0, buffer, NUM_OF_NEOPIXELS * 3);
 
     while(1) {
         uBit.display.scroll("HELLO WORLD!");
