e814806a10cc9d6b431ebbd165a2e86bc17e9ad9
Sam Kent <sam@microbit.org>
2021-01-19 16:15:00 +0000

    Add BLE samples

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include "MicroBit.h"
+#include "Tests.h"
 
 MicroBit uBit;
 
@@ -6,6 +7,7 @@ int
 main()
 {
     uBit.init();
+    ble_test();
 
     while(1)
         uBit.display.scroll("HELLO WORLD!");
