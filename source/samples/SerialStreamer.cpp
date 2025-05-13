674817573c0bdaa79d8da882c55f281e8851ecf1
Carlos Pereira Atencio <carlos@microbit.org>
2025-05-13 13:26:32 +0100

    SerialStreamer has been moved to codal-core.
    And the version in codal-core includes a bug fix required in the
    audio-refactor branch.

--- source/samples/SerialStreamer.cpp
+++ source/samples/SerialStreamer.cpp
@@ -21,126 +21,15 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
-
+#include "MicroBit.h"
 #include "SerialStreamer.h"
 #include "Tests.h"
 
-/**
- * Creates a simple component that logs a stream of signed 16 bit data as signed 8-bit data over serial.
- * @param source a DataSource to measure the level of.
- * @param mode the format of the serialised data. Valid options are SERIAL_STREAM_MODE_BINARY (default), SERIAL_STREAM_MODE_DECIMAL, SERIAL_STREAM_MODE_HEX.
- */
-SerialStreamer::SerialStreamer(DataSource &source, int mode) : upstream(source)
-{
-    this->mode = mode;
+void streamer_serial_test() {
+    static SplitterChannel *splitterChannel = uBit.audio.splitter->createChannel();
+    SerialStreamer *streamer = new SerialStreamer(*splitterChannel, SERIAL_STREAM_MODE_DECIMAL);
 
-    // Register with our upstream component
-    source.connect(*this);
-}
-
-/**
- * Callback provided when data is ready.
- */
-int SerialStreamer::pullRequest()
-{
-    static volatile int pr = 0;
-     
-    if(!pr)
-    {
-        pr++;
-        while(pr)
-        {
-            lastBuffer = upstream.pull();
-            streamBuffer(lastBuffer);
-            pr--;
-        }
-    }
-    else
-    {
-        pr++;
-    }
-    
-    return DEVICE_OK;
-}
-
-/**
-    * returns the last buffer processed by this component
-    */
-ManagedBuffer SerialStreamer::getLastBuffer()
-{
-    return lastBuffer;
-}
-
-/**
- * Callback provided when data is ready.
- */
-void SerialStreamer::streamBuffer(ManagedBuffer buffer)
-{
-    int CRLF = 0;
-    int bps = upstream.getFormat();
-
-    // If a BINARY mode is requested, simply output all the bytes to the serial port.
-    if( mode == SERIAL_STREAM_MODE_BINARY )
-    {
-        uint8_t *p = &buffer[0];
-        uint8_t *end = p + buffer.length();
-
-        while(p < end)
-            uBit.serial.putc(*p++);
-    }
-
-    // if a HEX mode is requested, format using printf, framed by sample size..
-    if( mode == SERIAL_STREAM_MODE_HEX || mode == SERIAL_STREAM_MODE_DECIMAL )
-    {
-        uint8_t *d = &buffer[0];
-        uint8_t *end = d+buffer.length();
-        uint32_t data;
-
-        while(d < end)
-        {
-            data = *d++;
-
-            if (bps > DATASTREAM_FORMAT_8BIT_SIGNED)
-                data |= (*d++) << 8;
-            if (bps > DATASTREAM_FORMAT_16BIT_SIGNED)
-                data |= (*d++) << 16;
-            if (bps > DATASTREAM_FORMAT_24BIT_SIGNED)
-                data |= (*d++) << 24;
-
-            if (mode == SERIAL_STREAM_MODE_HEX) {
-                uBit.serial.printf("%x ", data);
-            } else {
-                // SERIAL_STREAM_MODE_DECIMAL
-                if (bps == DATASTREAM_FORMAT_8BIT_SIGNED) {
-                    uBit.serial.printf("%d ", (int8_t)(data & 0xFF));
-                } else if (bps == DATASTREAM_FORMAT_16BIT_SIGNED) {
-                    uBit.serial.printf("%d ", (int16_t)(data & 0xFFFF));
-                } else if (bps == DATASTREAM_FORMAT_24BIT_SIGNED) {
-                    // Move the sign bit to the most significant bit
-                    int32_t signed_data = data & 0x7FFFFF;
-                    if (data & (1 << 23)) {
-                        signed_data |= 1 << 31;
-                    }
-                    uBit.serial.printf("%d ", signed_data);
-                } else {
-                    // Cannot print uint32_t correctly as serial.printf
-                    // does not support "%u"
-                    uBit.serial.printf("%d ", data);
-                }
-            }
-
-            CRLF++;
-
-            if (CRLF >= 16){
-                uBit.serial.putc('\r');
-                uBit.serial.putc('\n');
-                CRLF = 0;
-            }
-        }
-
-        if (CRLF > 0) {
-            uBit.serial.putc( '\r' );
-            uBit.serial.putc( '\n' );
-        }
+    while (true) {
+        uBit.sleep(1000);
     }
 }
